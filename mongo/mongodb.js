f26cdb7260adc0acf842ea4ef671192a7d6495e7
Hassen Khalifa <khalifa.hassen.pro@gmail.com>
2022-04-17 21:39:10 +0200

    test pour trouver un user en particulier

--- mongo/mongodb.js
+++ mongo/mongodb.js
@@ -1,29 +1,30 @@
 var axios = require('axios');
 var data = JSON.stringify({
     "collection": "Data",
+    "filter": {
+        "info.user":'dfdfddf11996'
+    },
     "database": "DataMonitoring",
     "dataSource": "Cluster0",
     "projection": {
-        "_id": 1,
-        "status":1,
-        "info":1
+        "info": 1
     }
 });
-            
+
 var config = {
     method: 'post',
-    url: 'https://data.mongodb-api.com/app/data-grsmg/endpoint/data/beta/action/findOne',
+    url: 'https://data.mongodb-api.com/app/data-grsmg/endpoint/data/beta/action/find',
     headers: {
         'Content-Type': 'application/json',
         'Access-Control-Request-Headers': '*',
         'api-key': 'pP3dYqzJQwzxhlj24CfIKPTHAy8LcAf4W2YdpRXbvmEXhh8LHLNOGSAabuO5QS7k'
     },
-    data : data
+    data: data
 };
-            
+
 axios(config)
     .then(function (response) {
-        console.log(JSON.stringify(response.data));
+        console.log(JSON.stringify(response.data.documents[0].info.user));
     })
     .catch(function (error) {
         console.log(error);

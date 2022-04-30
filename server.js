234ac2ae87fd94532336cef4a860a09bd9fdd8be
Hassen Khalifa <khalifa.hassen.pro@gmail.com>
2022-04-30 18:24:15 +0200

    corss app

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
+const cors = require('cors')
 app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(serveStatic(path.join(__dirname, 'dist')),cors());
 const port = process.env.PORT || 3000;
 app.listen(port);
